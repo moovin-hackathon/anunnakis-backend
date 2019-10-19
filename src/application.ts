cba6553af1013532438eae64945b11a54e932395
Thiago Hofmeister <thiago.hofmeister@gmail.com>
2019-10-19 18:02:52 -0300

    Adiciona middlewares

--- src/application.ts
+++ src/application.ts
@@ -2,7 +2,7 @@ import * as express from 'express'
 import * as cors from 'cors'
 import * as dotenv from 'dotenv'
 import { ItemDetail } from './Response'
-import { connectToDatabase } from './Middleware'
+import { HandlerFactory } from './Factory'
 
 (async () => {
   dotenv.config()
@@ -12,11 +12,15 @@ import { connectToDatabase } from './Middleware'
   application.use(express.json())
   application.use(express.urlencoded({ extended: false }))
   application.use(cors())
-  application.use(connectToDatabase)
+  // application.use(connectToDatabase)
 
   application.get('/status', (request, response, next) => {
     next(new ItemDetail())
   })
+
+  application.use(HandlerFactory.getSuccessHandler().success)
+  application.use(HandlerFactory.getErrorHandler().error)
+  application.use(HandlerFactory.getErrorHandler().notFound)
 
   application.set('port', process.env.APP_PORT || 3000)
 
